60c28235f7af6bdb6441d2749dd16dff440773cf
marvinO1 <git@atberis.ch>
2014-08-11 22:27:54 +0200

    adding configuration

---
+++ tasklist-server/tasklist-server-dao/src/main/java/org/rib/tasklist/dao/task/StorageType.java
@@ -0,0 +1,7 @@
+package org.rib.tasklist.dao.task;
+
+public enum StorageType {
+	FILE,
+	NEO4J;
+}
+
--- tasklist-server/tasklist-server-rest/src/main/java/org/rib/tasklist/rest/TaskListConfiguration.java
+++ tasklist-server/tasklist-server-rest/src/main/java/org/rib/tasklist/rest/TaskListConfiguration.java
@@ -6,29 +6,74 @@ import java.nio.file.Paths;
 import org.rib.tasklist.api.Task;
 import org.rib.tasklist.api.User;
 import org.rib.tasklist.dao.task.GenericDao;
+import org.rib.tasklist.dao.task.StorageType;
 import org.rib.tasklist.dao.task.impl.file.GenericDaoFile;
 import org.rib.tasklist.services.api.TaskListService;
 import org.rib.tasklist.services.impl.TaskListServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
+
 public class TaskListConfiguration {
 
-	private final Path rootPath = Paths.get(".");
-		
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	public GenericDao<Task> taskDao() {
-		return new GenericDaoFile<Task>(rootPath.resolve("tasks"));
+		StorageType storage = getStorageType();
+		switch (storage) {
+		case FILE:
+			return getFileTaskDao();
+		case NEO4J:
+			return getNeo4JTaskDao();
+		default:
+			throw new IllegalStateException("Given storage=" + storage + " is not known!");
+		}
 	}
 	
 	@Bean
 	public GenericDao<User> userDao() {
-		return new GenericDaoFile<User>(rootPath.resolve("users"));
+		StorageType storage = getStorageType();
+		switch (storage) {
+		case FILE:
+			return getFileUserDao();
+		case NEO4J:
+			return getNeo4JUserDao();
+		default:
+			throw new IllegalStateException("Given storage=" + storage + " is not known!");
+		}
 	}
 	
 	@Bean
 	public TaskListService taskListService() {
 		return new TaskListServiceImpl(taskDao(), userDao());
 	}
+	
+	protected GenericDao<Task> getNeo4JTaskDao() {
+		return new GenericDaoFile<Task>(getFileRootPath().resolve("tasks"));
+	}
+	
+	protected GenericDao<User> getNeo4JUserDao() {
+		return new GenericDaoFile<User>(getFileRootPath().resolve("tasks"));
+	}
+	
+	protected GenericDao<Task> getFileTaskDao() {
+		return new GenericDaoFile<Task>(getFileRootPath().resolve("tasks"));
+	}
+	
+	protected GenericDao<User> getFileUserDao() {
+		return new GenericDaoFile<User>(getFileRootPath().resolve("users"));
+	}
+		
+	protected Path getFileRootPath() {
+		return Paths.get(this.env.getProperty("storage.file.rootpath", "."));
+	}
+	
+	protected StorageType getStorageType() {
+		return StorageType.valueOf(this.env.getProperty("storage.type", "file").toUpperCase());
+	}	
 }
